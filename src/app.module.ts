9922f9cb19b1b8b5e763b89547a733d192ff6bc7
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-16 10:05:59 +0300

    added pino

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerModule } from './logger/logger.module';
+import { LoggerModule } from 'nestjs-pino';
+import { Request, Response } from 'express';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        customProps: (req: Request, res: Response) => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+        },
+      },
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from './config';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   await app.listen(PORT);
 }
 bootstrap();
